f144bba52a03db5e96684ae1676d64d9b357cd78
Rahulchunduruu <rahulchunduruu@gmail.com>
2026-02-17 16:43:07 +0530

    Fix indentation error in Config.py

--- Config.py
+++ Config.py
@@ -1,10 +1,13 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 
 load_dotenv()
+
 class config:
-        Grok_api=os.getenv('grok_api')
-        Hugging_face_api=os.getenv('hugging_face_api')
-        OPENAI_API_KEY=os.getenv('OPENAI_API_KEY')
-        vector_store_path=os.getenv('vector_store_path')
+    #If you prefer to run this in the terminal, make sure the first line is active and the second is commented out
+    #GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+    GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
+    if not GEMINI_API_KEY:
+        raise ValueError("GEMINI_API_KEY not found in .env file. Please add your API key.")

f30a35537b9a00e617a671d8f6eae3467716f51c
Rahul <rahulchunduruu@gmail.com>
2026-02-17 16:45:06 +0530

    Fix indentation for error handling in Config.py

--- Config.py
+++ Config.py
@@ -7,6 +7,6 @@ load_dotenv()
 class config:
         #If you prefer to run this in the terminal, make sure the first line is active and the second is commented out
         #OPENAI_API_KEY=os.getenv('OPENAI_API_KEY')
-        OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+       OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
        if not OPENAI_API_KEY:
-        raise ValueError("GEMINI_API_KEY not found in .env file. Please add your API key.")
+                raise ValueError("GEMINI_API_KEY not found in .env file. Please add your API key.")
